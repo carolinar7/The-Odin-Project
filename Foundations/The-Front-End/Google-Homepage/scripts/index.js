8f7919f7077197e223ad97b026a0edc112ec145a
Freddy <frsantiago2001@gmail.com>
2021-04-26 23:01:33 -0400

    Add search capability

--- Foundations/The-Front-End/Google-Homepage/scripts/index.js
+++ Foundations/The-Front-End/Google-Homepage/scripts/index.js
@@ -1,3 +1,13 @@
 function signIn() {
   location.href = "https://accounts.google.com/signin";
 }
+
+function search() {
+  let searchContent = document.getElementById("search-textarea").value;
+  let searchURL = "https://www.google.com/search?q=" + searchContent;
+  location.href = searchURL;
+}
+
+window.addEventListener("pageshow", () => {
+  document.getElementById("search-textarea").value = "";
+});
