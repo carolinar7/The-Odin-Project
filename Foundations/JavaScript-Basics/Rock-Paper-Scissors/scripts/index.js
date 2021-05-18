9f02462e9c9c1e4277b07a50439730ff518b1bf4
Freddy <frsantiago2001@gmail.com>
2021-05-18 13:36:53 -0400

    Add best of 5 functionality

--- Foundations/JavaScript-Basics/Rock-Paper-Scissors/scripts/index.js
+++ Foundations/JavaScript-Basics/Rock-Paper-Scissors/scripts/index.js
@@ -2,6 +2,9 @@ const SPIDERMAN = 1;
 const IRONMAN = 2;
 const THANOS = 3;
 const MAX_TYPES = 3;
+const PLAYER_WINNER = 'You win the best of 5!'
+const COMPUTER_WINNER = 'You lost the best of 5 :('
+var play = true;
 var playerScore = 0;
 var computerScore = 0;
 
@@ -9,36 +12,64 @@ function computerPlay() {
   return Math.ceil((Math.random() * MAX_TYPES));
 }
 
-function playRound(computerChoice, playerChoice) {
-  if (computerChoice == playerChoice) {
-    return 'Tie!';
-  } else if (computerChoice == SPIDERMAN && playerChoice == THANOS) {
-    playerScore += 1;
-    return 'You Win! Thanos beats Spider-Man';
-  } else if (computerChoice == SPIDERMAN && playerChoice == IRONMAN) {
-    computerScore += 1;
-    return 'You Lose! Spider-Man beats Iron Man';
-  } else if (computerChoice == IRONMAN && playerChoice == SPIDERMAN) {
-    playerScore += 1;
-    return 'You Win! Spider-Man beats Iron Man';
-  } else if (computerChoice == IRONMAN && playerChoice == THANOS) {
-    computerScore += 1;
-    return 'You Lose! Iron Man beats Thanos';
-  } else if (computerChoice == THANOS && playerChoice == SPIDERMAN) {
-    computerScore += 1;
-    return 'You Lose! Thanos beats Spider-Man';
-  } else {
-    playerScore += 1;
-    return 'You Win! Iron Man beats Thanos';
+function playerPlay(selection) {
+  if (play === false) {
+    return;
   }
+
+  printResult(playRound(computerPlay(), selection))
+  updateScores();
+}
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice == playerChoice) {
+      return 'Tie!';
+    } else if (computerChoice == SPIDERMAN && playerChoice == THANOS) {
+      playerScore += 1;
+      return 'You Win! Thanos beats Spider-Man';
+    } else if (computerChoice == SPIDERMAN && playerChoice == IRONMAN) {
+      computerScore += 1;
+      return 'You Lose! Spider-Man beats Iron Man';
+    } else if (computerChoice == IRONMAN && playerChoice == SPIDERMAN) {
+      playerScore += 1;
+      return 'You Win! Spider-Man beats Iron Man';
+    } else if (computerChoice == IRONMAN && playerChoice == THANOS) {
+      computerScore += 1;
+      return 'You Lose! Iron Man beats Thanos';
+    } else if (computerChoice == THANOS && playerChoice == SPIDERMAN) {
+      computerScore += 1;
+      return 'You Lose! Thanos beats Spider-Man';
+    } else {
+      playerScore += 1;
+      return 'You Win! Iron Man beats Thanos';
+    }
+}
+
+function playAgain() {
+  const playAgainDiv = document.getElementById('play-again');
+  const playAgainBtn = document.createElement("BUTTON");
+  playAgainBtn.innerHTML = 'Play Again';
+  playAgainBtn.addEventListener('click', () => location.reload());
+  playAgainDiv.appendChild(playAgainBtn);
 }
 
 function printResult(str) {
   const resultDiv = document.getElementById('game-result');
+
   if (resultDiv.hasChildNodes()) {
     resultDiv.removeChild(resultDiv.childNodes[0]);
   }
+
+  if (playerScore === 5 || computerScore === 5) {
+    str = (playerScore === 5) ? PLAYER_WINNER : COMPUTER_WINNER;
+    play = false;
+  }
+
   resultDiv.appendChild(document.createTextNode(str));
+
+  if (play === false) {
+    playAgain();
+  }
 }
 
 function updateScores() {
@@ -49,19 +80,10 @@ function updateScores() {
 }
 
 const spidermanImg = document.getElementById('spiderman');
-spidermanImg.addEventListener('click', () => {
-  printResult(playRound(computerPlay(), SPIDERMAN))
-  updateScores();
-});
+spidermanImg.addEventListener('click', () => playerPlay(SPIDERMAN));
 
 const ironmanImg = document.getElementById('ironman');
-ironmanImg.addEventListener('click', () => {
-  printResult(playRound(computerPlay(), IRONMAN))
-  updateScores();
-});
+ironmanImg.addEventListener('click', () => playerPlay(IRONMAN));
 
 const thanosImg = document.getElementById('thanos');
-thanosImg.addEventListener('click', () => {
-  printResult(playRound(computerPlay(), THANOS))
-  updateScores();
-});
+thanosImg.addEventListener('click', () => playerPlay(THANOS));
